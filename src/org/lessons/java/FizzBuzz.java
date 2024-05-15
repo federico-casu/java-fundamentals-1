5a6c8073b94894245c8de707d5431a7592456acb
federico-casu <federico.casu1101@gmail.com>
2024-05-15 15:48:15 +0200

    third snack: FizzBuzz

---
+++ src/org/lessons/java/FizzBuzz.java
@@ -0,0 +1,29 @@
+package org.lessons.java;
+
+public class FizzBuzz {
+    public static void main(String[] args) {
+        /*
+            Scrivi un programma che stampi i numeri da 1 a 100, ma per i multipli di 3 stampi “Fizz”
+            al posto del numero e per i multipli di 5 stampi Buzz. Per i numeri che sono sia multipli
+            di 3 che di 5 stampi FizzBuzz.
+         */
+
+        for (int i = 1; i <= 100; i++) {
+            if (i % 3 == 0 && i % 5 ==0) {
+                System.out.print(" FizzBuzz");
+                if (i % 10 == 0) {
+                    System.out.print("\n");
+                }
+            } else if (i % 3 == 0) {
+                System.out.print(" Fizz");
+            } else if (i % 5 == 0) {
+                System.out.print(" Buzz");
+                if (i % 10 == 0) {
+                    System.out.print("\n");
+                }
+            } else {
+                System.out.print(" " + i);
+            }
+        }
+    }
+}
