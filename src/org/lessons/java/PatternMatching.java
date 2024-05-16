02decbfb0c3db58a14146f3e2f2645837ba12dde
federico-casu <federico.casu1101@gmail.com>
2024-05-16 17:39:51 +0200

    fix: fixed PatternMatching managing if the character is not found

--- src/org/lessons/java/PatternMatching.java
+++ src/org/lessons/java/PatternMatching.java
@@ -17,7 +17,7 @@ public class PatternMatching {
 
         String string;
         char userChar;
-        int oldIndex, oldCount, newIndex, newCount;
+        int tempIndex, tempCount, newIndex, newCount;
 
         Scanner scanner = new Scanner(System.in);
 
@@ -26,35 +26,41 @@ public class PatternMatching {
         System.out.println("Inserisci il carattere da cercare: ");
         userChar = scanner.next().charAt(0);
 
-        oldCount = 0;
+        tempCount = 0;
         newCount = 0;
-        oldIndex = 0;
-        newIndex = 0;
+        tempIndex = -1;
+        newIndex = -1;
 
-        for (int i = 0; i < string.length(); i++) {
+        for (int i = 0; i < string.length(); i++) { // scorro l'array
+            // se il carattere della stringa in posizione i é uguale al carattere ceercato
             if (string.charAt(i) == userChar) {
-                newCount++;
-                if (i == 0 || string.charAt(i-1) != userChar) {
-                    newIndex = i;
+                newCount++; // incremento il contatore
+                // se stiamo confrontando la prima lettera della stringa o se la lettera precedente (la i-1esima) é diversa da quella cercata
+                if (i == 0 || string.charAt(i-1) != userChar) { // se
+                    newIndex = i; // aggiorno l'indice con la posizione corrente
                 }
-            } else {
-                if (newCount > oldCount) {
-                    oldCount = newCount;
-                    oldIndex = newIndex;
+            } else { // altrimenti
+                if (newCount >= tempCount) { // se il nuovo contatore é maggiore o uguale del contatore backup del precedente conteggio
+                    tempCount = newCount; // aggiorno il contatore backup con il nuovo conteggio
+                    tempIndex = newIndex; // aggiorno l'indice backup con il nuovo indice
                 }
-                newCount = 0;
-                newIndex = 0;
+                newCount = 0;   // azzero il nuovo contatore
+                newIndex = -1;  // azzero il nuovo indice
             }
         }
 
         System.out.println("Stringa: " + string);
         System.out.println("Carattere cercato: " + userChar);
-        if (oldCount > newCount) {
-            System.out.println("Lunghezza massima: " + oldCount);
-            System.out.println("Indice: " + oldIndex);
-        } else {
+        if (tempIndex == -1 && newIndex == -1) {    // se gli indici sono ancora nulli
+            System.out.println("Carattere non trovato");    // il carattere non é mai stato trovato
+        } else if (tempCount > newCount) {  // se il contatore di backup é maggiore dell'ultimo contatore
+            System.out.println("Lunghezza massima: " + tempCount);
+            System.out.println("Indice: " + tempIndex);
+        } else {    // altrimenti
             System.out.println("Lunghezza massima: " + newCount);
             System.out.println("Indice: " + newIndex);
         }
+
+        scanner.close();
     }
 }
